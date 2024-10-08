8e8ce5b58746cc0e671ef98402aec922741accd4
Zane Chee <zane.chee.2023@scis.smu.edu.sg>
2024-10-08 16:36:18 +0800

    added horizontal scrolling

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,13 +1,30 @@
+// pages/Home.tsx or pages/index.tsx
+
 import Image from "next/image";
-import bg from "@/app/assets/websitebg.png";
+import img from "@/public/weed.webp";
+import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
 
 export default function Home() {
   return (
-    <div className="flex flex-row justify-center h-dvh p-5">
-      <h1> DRUGS ARE BAD </h1>
-      <div className=""><Image src={bg} alt="Graph Illustration" width={500} height={300} /></div>
-  
-      <h3> this is your lungs on drugs </h3>
+    <div className="w-full h-full" style={{ maxHeight: '100vh' }}>
+      <ScrollArea className="w-full h-full">
+        <div className="flex flex-row items-center space-x-4 p-4 min-w-max" style={{ minHeight: '96vh' }}>
+        <h1 className="text-2xl font-bold whitespace-nowrap">DRUGS ARE BAD</h1>
+          <div className="flex-shrink-0">
+            <Image src={img} alt="Graph Illustration" width={500} height={300} />
+          </div>
+          <h3 className="text-lg whitespace-nowrap">This is your lungs on drugs</h3>
+          {/* Add more content to enable horizontal scrolling */}
+          <h3 className="text-lg whitespace-nowrap">More Content Here</h3>
+          <h3 className="text-lg whitespace-nowrap">More Content Here</h3>
+          <h3 className="text-lg whitespace-nowrap">More Content Here</h3>
+          <h3 className="text-lg whitespace-nowrap">More Content Here</h3>
+          <h3 className="text-lg whitespace-nowrap">More Content Here</h3>
+          <h3 className="text-lg whitespace-nowrap">More Content Here</h3>
+        </div>
+        {/* Horizontal ScrollBar */}
+        <ScrollBar orientation="horizontal" className="mt-2" />
+      </ScrollArea>
     </div>
   );
 }
--- my-app/components/navbar/navigation-bar.tsx
+++ my-app/components/navbar/navigation-bar.tsx
@@ -1,37 +1,46 @@
+// components/navbar/navigation-bar.tsx
+
 import Link from "next/link";
-import { NavigationMenu, NavigationMenuItem, NavigationMenuLink, navigationMenuTriggerStyle } from "../ui/navigation-menu";
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  navigationMenuTriggerStyle,
+} from "../ui/navigation-menu";
 import { ModeToggle } from "@/components/modetoggle";
 
 export function NavigationBar() {
-    return (
-        <>
-        <NavigationMenu className="flex flex-row justify-between min-w-full sticky top-0 list-none h-16 p-5">
-            <div className="flex flex-row">
-            <NavigationMenuItem>
-                <Link href="/" legacyBehavior passHref>
-                    <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                        Home
-                    </NavigationMenuLink>
-                </Link>
-            </NavigationMenuItem>
-            <NavigationMenuItem>
-                <Link href="/about-me" legacyBehavior passHref>
-                    <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                        About me
-                    </NavigationMenuLink>
-                </Link>
-            </NavigationMenuItem>
-            <NavigationMenuItem>
-                <Link href="/quiz" legacyBehavior passHref>
-                    <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                        Quiz
-                    </NavigationMenuLink>
-                </Link>
-            </NavigationMenuItem>
-            </div>
-            <ModeToggle/>
-            
-        </NavigationMenu>
-        </>
-    )
+  return (
+    <NavigationMenu className="sticky top-0 z-50 flex flex-row justify-between items-center w-full h-16 px-5">
+      {/* Navigation Items */}
+      <div className="flex flex-row space-x-4 overflow-x-auto">
+        <NavigationMenuItem>
+          <Link href="/" passHref legacyBehavior>
+            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+              Home
+            </NavigationMenuLink>
+          </Link>
+        </NavigationMenuItem>
+        {/* Add more NavigationMenuItem components as needed */}
+        <NavigationMenuItem>
+          <Link href="/about-me" passHref legacyBehavior>
+            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+              About Me
+            </NavigationMenuLink>
+          </Link>
+        </NavigationMenuItem>
+        <NavigationMenuItem>
+          <Link href="/quiz" passHref legacyBehavior>
+            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+              Quiz
+            </NavigationMenuLink>
+          </Link>
+        </NavigationMenuItem>
+        {/* Add additional items to enable horizontal scrolling */}
+      </div>
+
+      {/* Mode Toggle */}
+      <ModeToggle />
+    </NavigationMenu>
+  );
 }
